c4906f54da3b55c9eb456de87ed1b5ebf8763807
Henrik Tran <henrik.tran@evimeria.se>
2024-06-03 10:41:06 +0200

    remove old try catch

--- src/api/shows.ts
+++ src/api/shows.ts
@@ -2,20 +2,8 @@ import axios from "axios";
 import { Show, ShowSearchResult } from "../types/show";
 
 export const getShows = async (query: string): Promise<ShowSearchResult[]> => {
-  try {
-    const { data } = await axios.get("search/shows?q=" + query);
-    return data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      // Handle Axios error
-      console.error("Axios error:", error.message);
-      return [];
-    } else {
-      // Handle other errors
-      console.error("Error:", error);
-      return [];
-    }
-  }
+  const { data } = await axios.get("search/shows?q=" + query);
+  return data;
 };
 
 export const getShowById = async (id: string): Promise<Show> => {
